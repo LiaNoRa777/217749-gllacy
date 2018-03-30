6db47627e84c26abb4e62f1d7889adb2a6bb4d90
LiaNoRa777 <36840260+LiaNoRa777@users.noreply.github.com>
2018-03-30 21:41:07 +0700

    js в отдельный файл

---
+++ js/script.js
@@ -0,0 +1,32 @@
+"use strict";          
+    var linkFeedback = document.querySelector(".js-modal");
+    var popup = document.querySelector(".modal");
+    var overlay = document.querySelector(".overlay");
+    var modalClose = document.querySelector(".modal-close");
+    var formFeedback = popup.querySelector("form");
+    var nameFeedback = popup.querySelector("[name=feedback-name]");
+    var emailFeedback = popup.querySelector("[name=feedback-email");
+    var messageFeedback = popup.querySelector("[name=feedback-message]");
+
+    linkFeedback.addEventListener("click", function (evt) {
+        evt.preventDefault();
+        popup.classList.add("modal-show");
+        overlay.classList.add("overlay-show");                
+        nameFeedback.focus();
+    });    
+
+    modalClose.addEventListener("click", function (evt) {
+        evt.preventDefault();
+        popup.classList.remove("modal-show");
+        overlay.classList.remove("overlay-show");
+        popup.classList.remove("modal-error")
+    });
+
+    formFeedback.addEventListener("submit", function (evt) {
+        if (!nameFeedback.value || !emailFeedback.value || !messageFeedback.value) {
+            evt.preventDefault();
+            popup.classList.remove("modal-error");
+            popup.offsetWidth = popup.offsetWidth;
+            popup.classList.add("modal-error");
+        }                 
+    });
